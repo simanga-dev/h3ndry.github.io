6b5ef49c8f93492bf7cb0f3e9a5110761bf98f6e
Hendry Khoza <h3ndry@users.noreply.github.com>
2022-12-29 07:13:32 +0200

    static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import md from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
