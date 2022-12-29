03a24454304ec0061449162c4b01868484ea3d41
Hendry Khoza <h3ndry@users.noreply.github.com>
2022-12-29 08:45:19 +0200

    prepare to deploy to gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,10 @@ const config = {
     ],
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
-        adapter: adapter(),
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
 
         prerender: {
             onError: 'continue'
