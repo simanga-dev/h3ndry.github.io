2d2f0385fc5d7258e320224053ee30e0e20e2596
Hendry Khoza <h3ndry@users.noreply.github.com>
2023-01-05 18:23:33 +0200

    * add fonts and images * added preprocess for markdown

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,22 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import md from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: vitePreprocess(),
+
+    extensions: ['.svelte', ...mdsvexConfig.extensions],
+    preprocess: [
+        md.mdsvex(mdsvexConfig),
+        preprocess({
+            scss: {
+                prependData: '@use "src/variables.scss" as *;'
+            }
+        })
+    ],
 
     kit: {
         adapter: adapter(),
